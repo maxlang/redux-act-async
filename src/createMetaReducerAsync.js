40e0f8e8b9f115f2446b0a3faf53ddf0c3bef2ec
Max Lang <maxwell.g.lang@gmail.com>
2018-03-21 00:27:31 -0400

    another typo

--- src/createMetaReducerAsync.js
+++ src/createMetaReducerAsync.js
@@ -3,7 +3,7 @@ import createReducerAsync, { defaultsState } from './createReducerAsync';
 export default function createMetaReducerAsync(actionAsync, defaultState = defaultsState) {
   const reducer = createReducerAsync(actionAsync, defaultsState);
 
-  return function(state, action) {
+  return function(state = {}, action) {
       switch (action.type) {
           case actionAsync.request:
           case actionAsync.ok:
